348a2369728c6907cc438975cf4f015032fdc793
mnyerczan <mnyerczan@outlook.hu>
2020-04-03 12:30:18 +0200

    first commit

--- Application/Core/functions.php
+++ Application/Core/functions.php
@@ -11,3 +11,14 @@ function getConfig($confPath)
     return json_decode( file_get_contents($confPath),true);
 }
 
+/**
+ * @param string $message Message about the error
+ */
+function errorLog($message)
+{   
+    
+    $path       = APPPATH.'Log/dberror.log';
+    $msg        = "[".date('Y-m-d H:i:s')."]".$message.PHP_EOL;
+
+    return file_put_contents($path, $msg);
+}
